c675dec238090dda8b31d91993fc19e7b1bb0a27
HopeCollector <cmw0249@gmail.com>
2025-06-11 22:25:09 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# zotero-link
+
+Zotero is a very popular academic management tool. Although it provides its own note-taking tool, many people still choose to take notes in Markdown using VSCode. One of the great features of Zotero's note-taking tool is that you can click on the extracted notes to return to the original text, and it also supports adding corresponding jump links to the copied content. These links look like this:
+
+```markdown
+“Specifically, if the distance between the current MAV position pc and the goal position G exceeds the planning horizon H, we project G onto a sphere centered at pc with a radius of H to obtain pg; otherwise, pg is set to G.” ([Ren 等, 2025, p. 14](zotero://select/library/items/YESAKMB6)) ([pdf](zotero://open-pdf/library/items/7DNHE65J?page=14&annotation=VU2CTMUA))
+```
+
+These links work properly in a browser, but not in VSCode. This plugin was created to solve this problem, allowing you to use Ctrl+left mouse click on Zotero links in Markdown notes to jump to the corresponding original text in Zotero.
+
+## Features
+
+1. Drag the original text into the note.
+2. Use **Ctrl + left mouse click** to jump to the original text.
+
+![usage](imgs/README/usage.gif)
+
+## Requirements
+
+……
+
+## Extension Settings
+
+……
+
+## Known Issues
+
+……
+
+## Release Notes
+
+Users see my release notes when I release a new version.
+
+### 1.0.0
+
+Initial release
+
+## Acknowledgements
+
+Thanks to @jorisvanzundert /[zotero-link-opener](https://github.com/jorisvanzundert/zotero-link-opener) for inspiration 
---
+++ src/extension.ts
@@ -0,0 +1,70 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import * as cp from 'child_process';
+
+const platformOpenCommand: Record<NodeJS.Platform, string> = {
+	win32: 'start ""',
+	darwin: 'open',
+	linux: 'xdg-open',
+	// The following platforms are not supported by this extension
+	// If you want to support these platforms, you can add the appropriate commands
+	// or use a library like `open` that abstracts the platform differences
+	aix: 'xdg-open',
+    freebsd: 'xdg-open',
+    android: 'xdg-open',
+    sunos: 'xdg-open',
+    haiku: 'xdg-open',
+    openbsd: 'xdg-open',
+    cygwin: 'xdg-open',
+    netbsd: 'xdg-open'
+};
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+	console.log('Jump Zotero is now active!');
+
+	const commandID = 'zotero-link.jump';
+
+	const jumpHandler = (uri: string) => {
+		if (!uri) {
+			vscode.window.showErrorMessage('No Zotero link provided.');
+			return;
+		}
+		const terminalCommand = `${platformOpenCommand[process.platform]} "${uri}"`;
+		cp.exec(terminalCommand, (err: Error | null, stdout: string, stderr: string) => {
+			// console.log('stdout: ' + stdout);
+			// console.log('stderr: ' + stderr);
+			if (err) {
+				vscode.window.showErrorMessage(`Could not open Zotero link: ${err}.`);
+			}
+		});
+	};
+
+
+	// Register a document link provider for markdown files to create clickable links
+	// that execute the command and pass the Zotero link as an argument
+	// This allows users to click on Zotero links in markdown files
+	// and open them directly in Zotero
+	const commandDisposable = vscode.commands.registerCommand(commandID, jumpHandler);
+	const clickDisposable = vscode.languages.registerDocumentLinkProvider('markdown', {
+        provideDocumentLinks(document, token) {
+            const text = document.getText();
+            const regex = /zotero:\/\/[&\w/?=-]+/g;
+            const links = [];
+            let match;
+            while ((match = regex.exec(text)) !== null) {
+                const start = document.positionAt(match.index);
+                const end = document.positionAt(match.index + match[0].length);
+                const range = new vscode.Range(start, end);
+                const commandUri = vscode.Uri.parse(
+                    `command:${commandID}?${encodeURIComponent(JSON.stringify(match[0]))}`
+                );
+                links.push(new vscode.DocumentLink(range, commandUri));
+            }
+            return links;
+        }
+    });
+	context.subscriptions.push(commandDisposable, clickDisposable);
+}
